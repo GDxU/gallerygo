6ba469f346bd9375da703f024c5cf05fdb3f36ee
hesby <jobhesk@gmail.com>
2016-10-26 15:43:47 +0800

    added line share button
    line share button

--- javascripts/installation.js
+++ javascripts/installation.js
@@ -5,5 +5,21 @@ var installation = {
   android: "https://github.com/GDxU/gallerygo/releases/download/0.14.9/GIX-20161023183524-v0.14.9.apk",
   UA: "UA-61080213-1",
   FB: "135624513168400",
-  KAO: "dcbeabcda78df18100c882cea12bc31c"
+  KAO: "dcbeabcda78df18100c882cea12bc31c",
+  script_cn: {
+    head: "用您最優秀的繪畫作品，推向市場并介紹給您的粉絲！",
+    create: "從利用AR技術在世界任何地方激發你的工作。上傳你的進度在您的個人收藏。",
+    engage: "建立自己的粉絲社區的藝術與讀者的喜歡。保持和激發你的創造者接受的文化藝術形式。張貼和分享內容到收藏卷。",
+    outreach: "尋找世界各地的最接近畫廊事件。激活在牆上或掛架滾動標籤通知你的探訪和分享您的發現到社區。",
+    sale: "全部或部分版權使用許可出售您的藝術品。",
+    report: "跟踪並與你的粉絲藝術社區優化您的作品。從整合原作品的藝術授權。"
+  },
+  script_en: {
+    head: "With your excellence master piece to make your fame!",
+    create: "Inspire your work from anywhere in the world using AR technology. Upload your progress to your profile collections.",
+    engage: "Build your fan art community with likes from the readers. Keep up and motivate your creators to embrace cultural art form. Post and share the content to the collections roll.",
+    outreach: "Find the closest gallery events across the world. Activate the tag on the wall or the hanger scroll to notify your visitation and share your discovery to the community",
+    sale: "Sell your artwork with full or partial copyright licensing.",
+    report: "Track and optimise your artwork with your fan art community. Consolidate the art licensing from original work."
+  }
 };
