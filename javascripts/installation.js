4443057ced58f106f1e94ebeedcdf6388a1d3906
hesby <jobhesk@gmail.com>
2016-10-26 13:55:14 +0800

    script fix

--- javascripts/installation.js
+++ javascripts/installation.js
@@ -2,5 +2,8 @@
  * Created by hesk on 16年10月26日.
  */
 var installation = {
-  android: "https://github.com/GDxU/gallerygo/releases/download/0.14.9/GIX-20161023183524-v0.14.9.apk"
+  android: "https://github.com/GDxU/gallerygo/releases/download/0.14.9/GIX-20161023183524-v0.14.9.apk",
+  UA: "UA-61080213-1",
+  FB: "135624513168400",
+  KAO: "dcbeabcda78df18100c882cea12bc31c"
 };
