cb8cc642973a922b34228d28df858308b0c2cde0
hesby <jobhesk@gmail.com>
2016-11-21 16:45:44 +0800

    upload

--- js/custom.js
+++ js/custom.js
@@ -145,5 +145,5 @@ function initialize() {
   });
 }
 
-google.maps.event.addDomListener(window, "load", initialize);
+//google.maps.event.addDomListener(window, "load", initialize);
 // ========== END GOOGLE MAP ========== //
